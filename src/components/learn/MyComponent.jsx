3b27accbbd2baa30f8f6e87cf742535da629ac35
hoangnv09 <vanhoang17091999@gmail.com>
2025-03-10 08:53:53 +0700

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,9 +1,28 @@
+import './style.css';
+// const hello = "xin chào"; //string
+// const hello = 5; //number
+// const hello = true; //boolean
+// const hello = undefined;
+// const hello = null;
+const hello = [1, 2, 3, 4, 5, 6, 7];
+// const hello = {
+//     name: "banana",
+//     age: 19
+// }
 const MyComponent = () => {
-    return(
-        <div>
-        my component
-    </div>
+    return (
+        <>
+            <div>
+                {/* {hello} world */}
+
+                {JSON.stringify(hello)} world
+            </div>
+            <div className='child'
+            style={{borderRadius: "10px" }}>
+                banana
+            </div>
+        </>
     )
-    }
+}
 
 export default MyComponent;
