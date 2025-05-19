41ff618f4335ee4572093ae90abe035e09fa40fc
hoangnv09 <vanhoang17091999@gmail.com>
2025-05-19 18:43:27 +0700

    video 36

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,15 +1,28 @@
 import './todo.css'
-const TodoNew = () => {
+const TodoNew = (props) => {
 
     console.log(">>> check point: ", props);
     const { addNewTodo}= props;
     
-    //addNewTodo("Hoang")
+    // addNewTodo("Hoang")
+    const handleClick =()=>{
+        alert("click me")
+    }
+
+    const handleOnChange =(name)=>{
+       console.log(">>>hihi", name);
+    }
     
     return (
         <div className="todo-input-group">
-            <input className="todo-input" type="text" placeholder="Enter your task" />
-            <button className="todo-add-btn">Add</button>
+            <input className="todo-input" type="text" placeholder="Enter your task" 
+            onChange={(event)=>handleOnChange(event.target.value)}
+            />
+            <button 
+                className="todo-add-btn" 
+                style={{cursor: "pointer"}}
+                onClick={handleClick}
+            >Add</button>
         </div>
     )
 }
